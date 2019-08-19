8fe0ec7e606d9e2ca94067845b1886edc93f6be0
Raphael Vidal Bernoldi <rbernoldi.mazzatech@ext.valid.com>
2019-08-19 12:00:00 -0300

    Implementacao de funcao de instalcao de docker compose

--- install_apps.sh
+++ install_apps.sh
@@ -102,6 +102,15 @@ function installDocker(){
 	sudo docker version
 
 	echo "Docker installed!"
+	
+	installDockerCompose 
+}
+
+function installDockerCompose(){
+	echo "Install docker compose"
+	sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	echo "docker-compose installed!"
 }
 
 
@@ -150,7 +159,7 @@ for param in $*; do
 	if [ $param == "docker"]; then
 		installDocker
 	fi
-
+	
 	if [ $param == "sublime-text"]; then
 		installSublime
 	fi
